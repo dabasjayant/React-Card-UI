3e35473de96708e98e68263da14ff13a8da156d0
Jayant Dabas <jayantd1112@gmail.com>
2020-11-28 17:59:34 +0800

    CSS-animation

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -11,16 +11,25 @@ function App() {
       <div className="card-stack">{addCards()}</div>
 
       <div className="controls">
-        <div className="skip noSelect"><img className="disableSave" src={skipIcon} alt="skipIcon" onContextMenu={(e)=> e.preventDefault()} /></div>
-        <div className="superlike noSelect"><img className="disableSave" src={superlikeIcon} alt="superlikeIcon" onContextMenu={(e)=> e.preventDefault()} /></div>
-        <div className="like noSelect"><img className="disableSave" src={likeIcon} alt="likeIcon" onContextMenu={(e)=> e.preventDefault()} /></div>
+        <div className="skip noSelect" onClick={skip}>
+          <img className="disableSave" src={skipIcon} alt="skipIcon" onContextMenu={(e)=> e.preventDefault()} />
+        </div>
+        <div className="superlike noSelect" onClick={superlike}>
+          <img className="disableSave" src={superlikeIcon} alt="superlikeIcon" onContextMenu={(e)=> e.preventDefault()} />
+        </div>
+        <div className="like noSelect" onClick={like}>
+          <img className="disableSave" src={likeIcon} alt="likeIcon" onContextMenu={(e)=> e.preventDefault()} />
+        </div>
       </div>
     </div>
   );
 }
 
+var keyId = 0;
+
 function addCards() {
   const newData = data.map((data, i) => {
+    keyId = i;
     return (
       <div className="card" key={i}>
         <img className="disableSave" src={process.env.PUBLIC_URL + "/assets/pokemon/" + data.img} alt="user" onContextMenu={(e)=> e.preventDefault()} />
@@ -29,6 +38,27 @@ function addCards() {
     )
   }) 
   return newData;
+}
+
+const like = function like() {
+  if (keyId >= 0) {
+    document.getElementsByClassName("card").item(keyId).classList.add("animateLike");
+    keyId -= 1;
+  }
+}
+
+const skip = function skip() {
+  if (keyId >= 0) {
+    document.getElementsByClassName("card").item(keyId).classList.add("animateSkip");
+    keyId -= 1;
+  }
+}
+
+const superlike = function superlike() {
+  if (keyId >= 0) {
+    document.getElementsByClassName("card").item(keyId).classList.add("animateSuperlike");
+    keyId -= 1;
+  }
 }
 
 export default App;
